f629e12231627a5ff5fc832b1449be4a2d1a84c2
Ahsung <dktjd2478@naver.com>
2020-06-29 02:32:07 +0900

    vision init

---
+++ app/src/main/java/com/example/vision/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.vision;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button gpsButton;
+    private Button cameraButton;
+    private Button detectButton;
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gpsButton = (Button) findViewById(R.id.gpsAct);
+        cameraButton = (Button) findViewById(R.id.cameraAct);
+        detectButton = (Button) findViewById(R.id.detectAct);
+
+        // GPS page Intent
+        gpsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),GpsActivity.class);
+                startActivity(intent);//액티비티 띄우기
+            }
+        });
+
+        // CAMERA page Intent
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),CameraActivity.class);
+                startActivity(intent);//액티비티 띄우기
+            }
+        });
+
+        // DETECTION page Intent
+        detectButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),DetectionActivity.class);
+                startActivity(intent);//액티비티 띄우기
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
